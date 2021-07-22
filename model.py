daf23af96aed7de9e8c78c3c3a45b7296ddd02c3
Joao Oliveira <joao.d.oliveira@gmail.com>
2021-07-22 10:06:23 -0300

    renamed variable fcs1_units to fcs1_units

--- model.py
+++ model.py
@@ -45,7 +45,7 @@ class Actor(nn.Module):
 class Critic(nn.Module):
     """Critic (Value) Model."""
 
-    def __init__(self, state_size, action_size, seed, fcs1_units=400, fc2_units=300):
+    def __init__(self, state_size, action_size, seed, fc1_units=400, fc2_units=300):
         """Initialize parameters and build model.
         Params
         ======
@@ -57,8 +57,8 @@ class Critic(nn.Module):
         """
         super(Critic, self).__init__()
         self.seed = torch.manual_seed(seed)
-        self.fcs1 = nn.Linear(state_size, fcs1_units)
-        self.fc2 = nn.Linear(fcs1_units+action_size, fc2_units)
+        self.fcs1 = nn.Linear(state_size, fc1_units)
+        self.fc2 = nn.Linear(fc1_units+action_size, fc2_units)
         self.fc3 = nn.Linear(fc2_units, 1)
         self.reset_parameters()
 
